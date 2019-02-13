6dab091139566bda3c14633d7900fa1daf1096d2
Henry Zhu <hi@henryzoo.com>
2019-02-13 09:51:04 -0500

    order

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -4,16 +4,16 @@ class Subscribe extends React.Component {
   render() {
     return (
       <p>
+        <a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9yc3Muc2ltcGxlY2FzdC5jb20vcG9kY2FzdHMvNzA0OS9yc3M%3D" target="_blank">
+          Google
+        </a>{' '}
+        &bull;{' '}
         <a href="https://itunes.apple.com/us/podcast/on-margins/id1236390557" target="_blank">
-          iTunes
+          Apple
         </a>{' '}
         &bull;{' '}
         <a href="https://open.spotify.com/show/5EXwiyKzPrrucncKyrHy0B" target="_blank">
           Spotify
-        </a>{' '}
-        &bull;{' '}
-        <a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9yc3Muc2ltcGxlY2FzdC5jb20vcG9kY2FzdHMvNzA0OS9yc3M%3D" target="_blank">
-          Google
         </a>{' '}
         &bull;{' '}
         <a href="https://rss.simplecast.com/podcasts/7049/rss" target="_blank" rel="noopener noreferrer">
